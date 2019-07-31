f65e3fab603fabe16977df298ff56896d86f3542
Autumn Chiu <autumnchiu@gmail.com>
2019-08-01 03:20:18 +0800

    imported apache poi to read and write excel files

--- PriceComparison/src/ProductOrganizer.java
+++ PriceComparison/src/ProductOrganizer.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ProductOrganizer {
-
+	
+	// late night thinkin: maybe this should come in an excel sheet 
+	// with the links, we read the links, and write to the same spreadsheet?
+	// could potentially gather other info from link too? ---> expansion idea
+	
+	
+	
+	
+	// probs: where would we read the file from? 
+	
+	ArrayList<Product> products;
+	
+	public ProductOrganizer(ArrayList<Product> products) {
+		this.products = products;
+		
+		// automatically sorts the products given in the arraylist
+		Collections.sort(products);
+		
+		//export to excel
+		this.writeToExcel();
+	}
+	
+	private void writeToExcel() {
+	
+		XSSFWorkbook workbook = new XSSFWorkbook();
+	}
+	
+	
 }
